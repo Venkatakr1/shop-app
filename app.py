dea5c4080503f3022adf598130cd5dd240bf06e8
VenkataKrishnaReddyTangirala <venkatakdnr@gmail.com>
2026-02-14 14:31:05 +0530

    ready for railway deploy

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
 import os
 import mysql.connector
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-# Railway MySQL connection
-db = mysql.connector.connect(
-    host=os.getenv("MYSQLHOST"),
-    port=os.getenv("MYSQLPORT"),
-    user=os.getenv("MYSQLUSER"),
-    password=os.getenv("MYSQLPASSWORD"),
-    database=os.getenv("MYSQLDATABASE")
-)
+# Railway or Local connection
+if "MYSQLHOST" in os.environ:
+    conn = mysql.connector.connect(
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER"),
+        password=os.environ.get("MYSQLPASSWORD"),
+        database=os.environ.get("MYSQLDATABASE"),
+        port=int(os.environ.get("MYSQLPORT"))
+    )
+else:
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Tangirala@2026",
+        database="shopdb"
+    )
+
+cursor = conn.cursor()
+
 
 @app.route("/")
 def home():
-    return "Shop app is running successfully 🚀"
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+    return render_template("index.html", products=products)
 
-@app.route("/testdb")
-def testdb():
-    cursor = db.cursor()
-    cursor.execute("SELECT DATABASE();")
-    result = cursor.fetchone()
-    return f"Connected to database: {result}"
+
+@app.route("/add", methods=["POST"])
+def add():
+    name = request.form["name"]
+    price = request.form["price"]
+    quantity = request.form["quantity"]
+
+    cursor.execute(
+        "INSERT INTO products (name, price, quantity) VALUES (%s, %s, %s)",
+        (name, price, quantity)
+    )
+    conn.commit()
+
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run()
